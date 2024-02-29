8f083fd9d2a4c70d98e215cb30a65f08ec7d4d2a
Job <jobgerjon@gmail.com>
2024-02-29 15:02:33 +0100

    some more fixes

--- app/geocoding.py
+++ app/geocoding.py
@@ -1,19 +1,40 @@
+import json
 import logging
 import os
+from typing import Optional
 
-from opencage.geocoder import OpenCageGeocode, RateLimitExceededError
+import httpx
+from typing_extensions import Tuple
+
+BASE_URL = "https://api.opencagedata.com/geocode/v1/json"
 
 logger = logging.getLogger(__name__)
 
 API_KEY = os.getenv("GEOCODING_API_KEY")
-geocoder = OpenCageGeocode(API_KEY)
 
 
-def get_lon_lat(address: str, city, country="Netherlands") -> dict:
-    try:
-        result = geocoder.geocode(",".join([address, city, country]))
-        if result and len(result):
-            return result[0]['geometry']
-    except RateLimitExceededError as ex:
-        logger.error(f"Rate limit exceeded: {ex}")
-        raise ex
+async def get_geocode(address: str, city, country="Netherlands") -> Optional[Tuple[float, float]]:
+    async with httpx.AsyncClient() as client:
+        response = await client.get(BASE_URL, params={"q": ",".join([address, city, country]), "key": API_KEY})
+
+    if response.status_code != 200:
+        raise Exception(f"Failed to fetch {BASE_URL}: {response.text}")
+
+    response = response.json()
+    rate = response.get("rate", {}).get("remaining", 0)
+    if rate < 10:
+        logger.warning(f"Geocoding API remaining allowed requests are low: {rate}")
+    else:
+        logger.debug(f"Geocoding API remaining allowed requests: {rate}")
+
+    result = response.get("results", [])
+    if len(result) == 0:
+        return None
+
+    return result[0]["geometry"]["lat"], result[0]["geometry"]["lng"]
+
+
+if __name__ == '__main__':
+    import asyncio
+
+    asyncio.run(get_geocode("Kerkstraat 1", "Amsterdam"))
--- app/main.py
+++ app/main.py
@@ -1,31 +1,51 @@
 import asyncio
 from datetime import datetime
+import logging
+
 
 import pandas as pd
 
 from app.data_handler import get_houses, put_houses, get_subscribers
+from app.geocoding import get_geocode
 from app.scraper import scrape
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+async def get_geocode_for_df(df, address):
+    row = df.loc[address]
+    if row["lat"] is not None and row["lon"] is not None:
+        return
+
+    pos = await get_geocode(row["address"], row["city"])
+    logger.info(f"Geocoded {row['address']}, {row['city']}: {pos}")
+
+    df.loc[address, ["lat", "lon"]] = pos
 
 
 async def main():
     scrape_start = datetime.now()
 
-    # old_df = await get_houses()
-    # new_df = pd.read_csv("temp.csv", index_col="address")
+    #old_df = await get_houses()
+    #new_df = pd.read_csv("temp.csv", index_col="address")
     new_df, old_df, subs = await asyncio.gather(scrape(), get_houses(), get_subscribers())
     new_df["scrape_date"] = pd.to_datetime(new_df['scrape_date'])
     old_df["scrape_date"] = pd.to_datetime(old_df['scrape_date'])
-    new_df.to_csv("temp.csv")
+    #new_df.head(-10).to_csv("temp.csv")
 
     await put_houses(pd.concat([new_df, old_df]).drop_duplicates(keep="last"))
     new_df = pd.concat([new_df, old_df]).drop_duplicates(keep=False)
     new_df = new_df.loc[new_df["scrape_date"] >= scrape_start]
 
-    for s in subs.iter_rows():
-        for i, row in new_df.iterrows():
-            if s["city"] == row["city"]:
-                print(f"Sending mail to {s['email']} for {row['address']}")
-                # send_mail(s["email"], row)
+    print(new_df)
+    await asyncio.gather(*[get_geocode_for_df(new_df, address) for address in new_df.head(5).index])
+
+    # for s in subs.iterrows():
+    #     for i, row in new_df.iterrows():
+    #         if s["city"] == row["city"]:
+    #             print(f"Sending mail to {s['email']} for {row['address']}")
+    #             # send_mail(s["email"], row)
 
 
 
